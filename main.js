18112d3ea61a0a7d3a3c2e183de3690ff4c13b9e
l.r0y <122097263+lroy-stack@users.noreply.github.com>
2023-11-25 18:31:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Smooth scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dynamic year in footer
+document.getElementById('currentYear').innerText = new Date().getFullYear();
